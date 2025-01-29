8a4614c7d7399c30e0d9242f1b76cfb5caac1a3b
JAmbrizH <lpl995175@gmail.com>
2025-01-29 14:41:29 -0700

    Updated SimpleRectangle Contents
    added "Hello World" to class

--- EclipseWorkSpace/cs2050ClassWork/LastnameTestSimpleRectangle.java
+++ EclipseWorkSpace/cs2050ClassWork/LastnameTestSimpleRectangle.java
@@ -1,17 +1,9 @@
-/**
- * 
- */
 
-/**
- * 
- */
 public class LastnameTestSimpleRectangle {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print("Hello World");
 
 	}
 
